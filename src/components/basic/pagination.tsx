2a99f65b604c466bb2e3db00ef9dd86f975980e1
Joe <joseph.rall@live.com>
2021-12-10 16:02:08 -0500

    Cleand up pagination.

--- src/components/basic/pagination.tsx
+++ src/components/basic/pagination.tsx
@@ -1,5 +1,3 @@
-import { faEllipsisH } from "@fortawesome/pro-duotone-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { generatePagination } from "@lib/pagination";
 import Link from "next/link";
 
@@ -12,27 +10,44 @@ type Props = {
   };
 };
 export default function Pagination({ current, pages, link }: Props) {
-  const pagination = generatePagination(current, pages);
+  //const pagination = generatePagination(current, pages);
+  const nextPage = current + 1;
+  const prevPage = current - 1;
   return (
-    <div className={"pagination justify-content-center"}>
-      {pagination.map((it, i) => (
-        <div
-          key={i}
-          className={`page-item ${it.page === current ? "active" : it.excerpt ? "disabled" : ""}`}
-        >
-          {it.excerpt ? (
-            <a className="page-link">
-              <FontAwesomeIcon icon={faEllipsisH} fixedWidth />
-            </a>
-          ) : it.page !== null ? (
-            <Link href={link.href(it.page)} as={link.as(it.page)}>
-              <a className={`page-link ${i === 0 || i === pagination.length - 1 ? "px-5" : ""}`}>
-                {it.page}
-              </a>
-            </Link>
-          ) : null}
-        </div>
-      ))}
+    <div className="d-flex flex-column">
+      <div className={"d-flex justify-content-between gap-5"}>
+        {current > 1 && (
+          <Link href={link.href(prevPage)} as={link.as(prevPage)}>
+            <a className={`btn btn-primary col`}>{"Newer"}</a>
+          </Link>
+        )}
+        {current < pages && (
+          <Link href={link.href(nextPage)} as={link.as(nextPage)}>
+            <a className={`btn btn-primary col`}>{"Older"}</a>
+          </Link>
+        )}
+      </div>
+      {/* <small className="align-self-center text-muted">{current}</small> */}
     </div>
+    //   <div className={"pagination justify-content-center"}>
+    //   {pagination.map((it, i) => (
+    //     <div
+    //       key={i}
+    //       className={`page-item ${it.page === current ? "active" : it.excerpt ? "disabled" : ""}`}
+    //     >
+    //       {it.excerpt ? (
+    //         <a className="page-link">
+    //           <FontAwesomeIcon icon={faEllipsisH} fixedWidth />
+    //         </a>
+    //       ) : it.page !== null ? (
+    //         <Link href={link.href(it.page)} as={link.as(it.page)}>
+    //           <a className={`page-link ${i === 0 || i === pagination.length - 1 ? "px-5" : ""}`}>
+    //             {it.page}
+    //           </a>
+    //         </Link>
+    //       ) : null}
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
