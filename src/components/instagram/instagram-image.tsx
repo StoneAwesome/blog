342ea5cd1ce3448955b5fc0735c1c8dc0672507b
Joe Rall <joseph.rall@live.com>
2021-12-15 15:29:06 -0500

    Made images smaller to improve performance

--- src/components/instagram/instagram-image.tsx
+++ src/components/instagram/instagram-image.tsx
@@ -1,10 +1,8 @@
 import type { StoredInstagramImage } from "@lib/instagram";
 import * as React from "react";
 import NextImage from "next/image";
-import {
-  buildInstagramThumbnailBlurImage,
-  buildSquareThumbnailImage,
-} from "@lib/image-service";
+import { buildInstagramThumbnailBlurImage, buildSquareThumbnailImage } from "@lib/image-service";
+import { useBootstrapBreaksDown } from "@hooks/use-media";
 
 export type InstagramImageProps = {
   image: StoredInstagramImage;
@@ -14,17 +12,19 @@ export type InstagramImageProps = {
 };
 
 const InstagramImage: React.FC<InstagramImageProps> = (props) => {
+  const size = useBootstrapBreaksDown(["md", "lg", "xl", "xxl"], [135, 164, 224, 238], 238);
+
   return (
     <NextImage
       src={props.image.id}
       className=""
       loader={(loaderProps) => buildSquareThumbnailImage(loaderProps.src, loaderProps.width)}
       alt={props.alt || "Image"}
-      height={420}
-      width={420}
+      height={size}
+      width={size}
       placeholder={"blur"}
       layout="responsive"
-      blurDataURL={buildInstagramThumbnailBlurImage(props.image.id, 420)}
+      blurDataURL={buildInstagramThumbnailBlurImage(props.image.id, size)}
     />
   );
 };
