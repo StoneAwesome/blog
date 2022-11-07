c833fb2983ea509be9699c08d201007505eccc04
Joe Rall <Joseph.Rall@Live.com>
2022-11-07 17:46:30 -0500

    Interim check-in

--- src/lib/storyblok/storyblok-material-client.ts
+++ src/lib/storyblok/storyblok-material-client.ts
@@ -1,17 +1,26 @@
 import StoryBlokClientBaseClass, {
-  IStoriesResponse,
+  IStoryBlokAssetMeta,
+  IStoryBlokContent,
 } from "@lib/storyblok/storyblok-client-base";
 
-class StoryBlokMaterialClientClass extends StoryBlokClientBaseClass<any> {
-  constructor(
-    token: string = process.env.NEXT_PUBLIC_STORYBLOK_READONLY_KEY,
-    isDraft: boolean = !!process.env.NEXT_PUBLIC_STORYBLOK_IS_DRAFT
-  ) {
+export interface IStoryBlokMaterialContent extends IStoryBlokContent {
+  name: string;
+  type: string;
+  images: IStoryBlokAssetMeta[];
+  component: string;
+  composition: string[];
+  description: string;
+  requires_sealing: boolean;
+  stain_resistance: string;
+}
+
+class StoryBlokMaterialClientClass extends StoryBlokClientBaseClass<IStoryBlokMaterialContent> {
+  constructor(token?: string, isDraft?: boolean) {
     super(token, isDraft);
   }
 
   async grabMaterialStories() {
-    const data = await this.executeCall<IStoriesResponse<any>>("cdn/stories", {
+    const data = await this.grabStories({
       starts_with: "material/",
       is_startpage: 1,
     });
--- src/pages/material/index.tsx
+++ src/pages/material/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { ReactText } from "react";
 import Layout from "@components/main-layout";
 import BasicMeta from "@components/meta/basic-meta";
 import OpenGraphMeta from "@components/meta/open-graph-meta";
 import TwitterCardMeta from "@components/meta/twitter-card-meta";
 import type { InferGetStaticPropsType, GetStaticProps } from "next";
-import InstagramList from "@components/instagram/instagram-list";
-import { createInstagramList } from "@lib/instagram";
-import StoryBlokClient from "@lib/storyblok/storyblok-blog-client";
-import StoryBlokMaterialClient from "@lib/storyblok/storyblok-material-client";
+import StoryBlokMaterialClient, {
+  IStoryBlokMaterialContent,
+} from "@lib/storyblok/storyblok-material-client";
+import type { IStoryBlockStory } from "@lib/storyblok/storyblok-client-base";
+import BasicContainer from "@components/basic/basic-container";
+import { StoryBlokImg } from "@components/storyblok/storyblok-image";
+import Link from "next/link";
+
+interface MaterialPageProps {
+  material: IStoryBlockStory<IStoryBlokMaterialContent>[];
+}
 
 export default function MaterialPage(
   props: //   posts,
@@ -17,33 +24,68 @@ export default function MaterialPage(
 ) {
   return (
     <Layout>
-      <BasicMeta url={"/"} />
+      <BasicMeta
+        url={"/"}
+        title={"Stone Library"}
+        description={"A world of stone"}
+      />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-      <div className="prose">
-        <pre>
-          <code>{JSON.stringify(props, null, 2)}</code>
-        </pre>
-      </div>
+      <BasicContainer noMB>
+        <div className="prose max-w-none">
+          <h1>{"The Stone Library"}</h1>
+          <p>
+            {
+              "Below you'll find a vast collection of stone, both natural and made made. Our goal with this library is to provide a comprehensive database of the most common materials used both in and around the home. It is by no means exhaustive, but it's a great start. If you don't see your material located there, or want to add your specific branded names please let us know."
+            }
+          </p>
+          <div className="not-prose grid grid-cols-3">
+            {props.material.map((m) => (
+              <MaterialArticle
+                key={m.uuid}
+                material={m.content}
+                slug={m.slug}
+              />
+            ))}
+          </div>
+        </div>
+      </BasicContainer>
     </Layout>
   );
 }
 
-//-- For now we'll just need to hard code these as it's easier.
+const MaterialArticle: React.FC<{
+  material: IStoryBlokMaterialContent;
+  slug: string;
+}> = ({ material: m, slug }) => {
+  const img = m.images?.[0];
+  return (
+    <div className="group relative flex flex-col rounded-xl border px-3 pt-1 pb-3">
+      <Link
+        href={`/material/${slug}`}
+        className="leading-2 text-center text-xl font-semibold after:absolute after:inset-0"
+      >
+        {m.name}
+      </Link>
+      {img ? (
+        <StoryBlokImg
+          img={img}
+          className={
+            "h-auto rounded transition-all ease-in-out group-hover:scale-105"
+          }
+        />
+      ) : null}
+    </div>
+  );
+};
 
-// const MAIN_MATERIAL_PAGES = [
-//   {
-//     name: "Granite",
-//     id: 142466688,
-//     uuid: "9cdd4373-91cc-4f16-9163-c1def44789c7",
-//   },
-// ];
-
-export const getStaticProps: GetStaticProps<{}> = async (ctx) => {
+export const getStaticProps: GetStaticProps<MaterialPageProps> = async (
+  ctx
+) => {
   const material = await StoryBlokMaterialClient.grabMaterialStories();
   return {
     props: {
-      materials: material,
+      material: material,
     },
   };
 };
