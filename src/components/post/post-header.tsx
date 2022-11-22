dad8f8dba1a8348e71716d3f2b7f91a4d9418297
Joe Rall <Joseph.Rall@Live.com>
2022-11-22 12:16:35 -0500

    Added share button

--- src/components/post/post-header.tsx
+++ src/components/post/post-header.tsx
@@ -1,4 +1,5 @@
 import DateView from "@components/basic/date-view";
+import ShareButton from "@components/basic/share-button";
 import { IBlogStoryMeta } from "@lib/storyblok/storyblok-blog-client";
 import type { IStoryBlockStory } from "@lib/storyblok/storyblok-client-base";
 import { parse } from "date-fns";
@@ -11,17 +12,24 @@ export type PostHeaderProps = {
 
 const PostHeader: React.FC<PostHeaderProps> = ({ post }) => {
   const publishDate = parse(post.content.date, "yyyy-MM-dd H:mm", new Date());
+  const url = `/blog/${post.slug}`;
   return (
-    <header className={"mt-5 mb-3"}>
-      <Link href={`/blog/${post.slug}`} className={"text-2xl text-_bsPrimary"}>
+    <header className={"mt-5 mb-3 grid grid-cols-2"}>
+      <Link
+        href={url}
+        className={"col-span-2 text-2xl text-_bsPrimary md:col-span-1"}
+      >
         <h1>{post.content.title}</h1>
       </Link>
-      <div className={"mb-4 flex items-center gap-4"}>
-        {/* <Author author={getAuthor(post.author)} /> */}
-        <div className={"flex items-center"}>
-          <DateView date={publishDate} />
-        </div>
+      <div className="mt-1 whitespace-nowrap">
+        <DateView date={publishDate} />
       </div>
+      <ShareButton
+        title={`${post.content.title} - StoneAwesome`}
+        className={
+          "my-auto ml-auto px-2 text-lg font-normal md:col-start-2 md:row-span-2 md:row-start-1"
+        }
+      />
     </header>
   );
 };
